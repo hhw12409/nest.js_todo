aa7aed1abadb3a8c92280c033f22189f8ef9129f
hhw12409 <sksk2425@naver.com>
2023-08-13 23:46:22 +0900

    feat: make User Model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TodoModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TodoModule, UserModule],
 })
 export class AppModule {}
