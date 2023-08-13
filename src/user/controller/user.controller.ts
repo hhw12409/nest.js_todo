213c70a3811bd7c8e01d324dab41ae91788b1dc3
hhw12409 <sksk2425@naver.com>
2023-08-14 00:20:30 +0900

    feat: all user, user by id api

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { UserService } from '../service/user.service';
+import { User } from '@prisma/client';
 
-@Controller('user')
-export class UserController {}
+@Controller('api/user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async getAllUsers(): Promise<User[]> {
+    return this.userService.getAllUsers();
+  }
+
+  @Get('/:id')
+  async getUserById(@Param('id') id: number): Promise<User> {
+    return this.userService.getUserById(id);
+  }
+}
--- src/user/service/user.service.ts
+++ src/user/service/user.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prismaService: PrismaService) {}
+
+  // 전체조회 쿼리빌더
+  async getAllUsers(): Promise<User[]> {
+    return this.prismaService.user.findMany();
+  }
+
+  // 단일조회 쿼리빌더
+  async getUserById(id: number): Promise<User> {
+    return this.prismaService.user.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+  }
+}
